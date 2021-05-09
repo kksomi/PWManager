18e0ee1a3ea27f07868f78a9a11a6d4b4802bbec
somi <nnn0111123@naver.com>
2021-05-10 05:13:39 +0900

    Password Page UI

--- app/src/main/java/com/example/pwmanager/PasswordFrag.java
+++ app/src/main/java/com/example/pwmanager/PasswordFrag.java
@@ -1,103 +1,57 @@
 package com.example.pwmanager;
 
-/* 수정 필요
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PasswordFrag extends Fragment {
 
-   private List<String> passwordList;          // 데이터를 넣은 리스트변수
-    private ListView listView;          // 검색을 보여줄 리스트변수
-    private EditText editSearch;        // 검색어를 입력할 Input 창
-    private com.example.pwmanager.SearchAdapter adapter;      // 리스트뷰에 연결할 아답터
-    private ArrayList<String> arraylist;
+    private RecyclerAdapter adapter;
+    private RecyclerView recyclerView;
+    private List<String> list = new ArrayList<>();
 
+
+
+    @Nullable
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);}
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.activity_password, container, false);
-
-        // 검색에 사용할 데이터을 미리 저장한다.
+        super.onCreate(savedInstanceState);
         settingList();
-
-        editSearch = (EditText) editSearch.findViewById(R.id.editSearch);
-        listView = (ListView) listView.findViewById(R.id.passwordList);
-
-        // 리스트를 생성한다.
-        passwordList = new ArrayList<String>();
-
-        // 리스트의 모든 데이터를 arraylist에 복사한다.// list 복사본을 만든다.
-        arraylist = new ArrayList<String>();
-        arraylist.addAll(passwordList);
-
-        // 리스트에 연동될 아답터를 생성한다.
-        adapter = new com.example.pwmanager.SearchAdapter(getActivity(), passwordList);
-
-        // 리스트뷰에 아답터를 연결한다.
-        listView.setAdapter(adapter);
-
-        // input창에 검색어를 입력시 "addTextChangedListener" 이벤트 리스너를 정의한다.
-        editSearch.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                // input창에 문자를 입력할때마다 호출된다.
-                // search 메소드를 호출한다.
-                String text = editSearch.getText().toString();
-                search(text);
-            }
-        });
-
-
-        return view;
-    }
-
-
-    // 검색을 수행하는 메소드
-    public void search(String charText) {
-
-        // 문자 입력시마다 리스트를 지우고 새로 뿌려준다.
-        passwordList.clear();
-
-        // 문자 입력이 없을때는 모든 데이터를 보여준다.
-        if (charText.length() == 0) {
-            passwordList.addAll(arraylist);
         }
-        // 문자 입력을 할때..
-        else {
-            // 리스트의 모든 데이터를 검색한다.
-            for (int i = 0; i < arraylist.size(); i++) {
-                // arraylist의 모든 데이터에 입력받은 단어(charText)가 포함되어 있으면 true를 반환한다.
-                if (arraylist.get(i).toLowerCase().contains(charText)) {
-                    // 검색된 데이터를 리스트에 추가한다.
-                    passwordList.add(arraylist.get(i));
-                }
-            }
-        }
-        // 리스트 데이터가 변경되었으므로 아답터를 갱신하여 검색된 데이터를 화면에 보여준다.
-        adapter.notifyDataSetChanged();
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.activity_password, container, false);
 
-    // 검색에 사용될 데이터를 리스트에 추가한다.
-    private void settingList() {
-        passwordList.add("1234");
-        passwordList.add("password");
-        passwordList.add("pw1234");
-        passwordList.add("1004");
+
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+
+        recyclerView.setHasFixedSize(true);
+        adapter = new RecyclerAdapter(getActivity(), list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(adapter);
+
+        Log.e("Frag", "PasswordFrag");
+        return rootView;
 
     }
+    void settingList(){
+        list.add("password");
+        list.add("qlalfqjsgh");
+        list.add("12345678");
+        list.add("secretNumber");
+        list.add("pwmanager");
+    }
+
 
 }
-
-*/
-
-

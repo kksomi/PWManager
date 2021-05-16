38137cb1f1b73caa7a6da81e47c34ff80ebf6c40
Hyerin0 <zzzao1231@gmail.com>
2021-05-16 19:46:23 +0900

    Login Page

---
+++ app/src/main/java/com/example/pwmanager/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.pwmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}

160877771eb9294fe33f641987b7b0f1c43aa3e1
Hansol-98 <ssoooooll98@gmail.com>
2021-05-18 12:19:20 +0900

    WebFrag

--- app/src/main/java/com/example/pwmanager/WebFrag.java
+++ app/src/main/java/com/example/pwmanager/WebFrag.java
@@ -22,13 +22,29 @@ import java.util.List;
 
 public class WebFrag extends Fragment {
 
+    private final ArrayList<String> list = null;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        throw new AssertionError();
+
+
     }
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_web, container, false);
 
+        RecyclerView recyclerView;
+        recyclerView = rootView.findViewById(R.id.recyclerView);
+
+        LinearLayoutManager layoutManager2 = new LinearLayoutManager(recyclerView.getContext());
+
+        recyclerView.setHasFixedSize(true);
+        RecyclerAdapter adapter = new RecyclerAdapter(getContext(), null);
+        recyclerView.setLayoutManager(layoutManager2);
+        recyclerView.setAdapter(adapter);
+
+        Log.e("Frag", "WebFrag");
 
         return rootView;
 
